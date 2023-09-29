66ab31e727ac44ec88d8a621bb8774651b71e687
chainreactionboom <145522791+chainreactionboom@users.noreply.github.com>
2023-09-29 13:04:04 -0400

    add Cleffa & families

--- data.js
+++ data.js
@@ -55,7 +55,8 @@ const Poke2Type = {
     'Magnezone': PTYPE.Skill,
     'Togekiss': PTYPE.Skill,
     'Leafeon': PTYPE.Skill, 'Glaceon': PTYPE.Skill, 'Sylveon': PTYPE.Skill,
-	'Mime Jr.': PTYPE.Ingredient, 'Mr. Mime': PTYPE.Ingredient
+	'Mime Jr.': PTYPE.Ingredient, 'Mr. Mime': PTYPE.Ingredient,
+    'Cleffa': PTYPE.Berry, 'Clefairy': PTYPE.Berry, 'Clefable': PTYPE.Berry,
 }
 const Nature2Score = {
     'Hardy': [1.00, 1.00, 1.00], 'Lonely': [1.00, 1.00, 0.67], 'Brave': [1.67, 1.67, 1.67],
